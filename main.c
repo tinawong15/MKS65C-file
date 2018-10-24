ed56e63f332f92a92cbe53b137606df568fd3dff
Michael Luo <33794837+RealMichael@users.noreply.github.com>
2018-10-23 20:55:34 -0400

    wrote write

--- main.c
+++ main.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 int main() {
-  int text_file = open("text.txt", O_RDWR);
   char * body = calloc(sizeof(char), 100);
+
+  int text_file = open("text.txt", O_RDWR);
+  printf("File Table Entry Number of Open: %d\n",text_file);
+  
   printf("===== TESTING READ =====\n");
   printf("Bytes read: %zd\n", read(text_file, body, 100));
   printf("%s\n", body);
+
   printf("===== TESTING WRITE =====\n");
   printf("===== TESTING IF WRITE IS SUCCESSFUL =====\n");
-  printf("Bytes read: %zd\n", read(text_file, body, 100));
+  printf("Bytes wrote: %zd\n", write(text_file,"MeAGoodWriter",strlen("MeAGoodWriter")));
   printf("%s\n", body);
   free(body);
   close(text_file);
