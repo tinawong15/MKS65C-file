c10f9d5b7dd292f33995a2ce4de2aea478b9325e
tina <tinaw425@gmail.com>
2018-10-23 21:14:32 -0400

    added test if write is successful

--- main.c
+++ main.c
@@ -5,20 +5,26 @@
 #include <string.h>
 
 int main() {
+  int text_file = open("text.txt", O_RDWR);
   char * body = calloc(sizeof(char), 100);
 
-  int text_file = open("text.txt", O_RDWR);
-  printf("File Table Entry Number of Open: %d\n",text_file);
-  
+  printf("File Table Entry Number of Open: %d\n", text_file);
+
   printf("===== TESTING READ =====\n");
+  printf("Bytes read: %zd\n", read(text_file, body, 100));
+  printf("%s", body);
+
+  printf("===== TESTING WRITE =====\n");
+  printf("Bytes written: %zd\n", write(text_file,"Me A Good Writer",strlen("Me A Good Writer")));
+
+  free(body);
+  close(text_file);
+
+  printf("===== TESTING IF WRITE IS SUCCESSFUL =====\n");
+  text_file = open("text.txt", O_RDONLY);
+  body = calloc(sizeof(char), 100);
   printf("Bytes read: %zd\n", read(text_file, body, 100));
   printf("%s\n", body);
 
-  printf("===== TESTING WRITE =====\n");
-  printf("===== TESTING IF WRITE IS SUCCESSFUL =====\n");
-  printf("Bytes wrote: %zd\n", write(text_file,"MeAGoodWriter",strlen("MeAGoodWriter")));
-  printf("%s\n", body);
-  free(body);
-  close(text_file);
   return 0;
 }
